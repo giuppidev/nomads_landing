252ea5e8a526e907546650896040b87146c9f228
Giuseppe Funicello <g.funicello@gmail.com>
2023-01-03 12:58:52 +0100

    add spinner and bigger text

--- src/pages/nomads.tsx
+++ src/pages/nomads.tsx
@@ -47,7 +47,7 @@ const MailchimpForm = ({
   return (
     <Form
       onSubmit={onSubmit}
-      render={({ handleSubmit, invalid, submitting }) => (
+      render={({ handleSubmit, invalid }) => (
         <div className="grid h-screen place-content-center content-center bg-white">
           <div className="mx-auto flex max-w-7xl flex-col gap-2 py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
             <h2 className="inline text-3xl font-bold tracking-tight text-gray-800 sm:block sm:text-4xl">
@@ -100,24 +100,50 @@ const MailchimpForm = ({
                     <button
                       disabled={true}
                       type="submit"
-                      className="flex w-full items-center justify-center rounded-md border border-transparent bg-teal-600 px-5 py-3 text-base font-medium text-white hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-teal-500 focus:ring-offset-2"
+                      className="flex w-full items-center justify-center rounded-md border border-transparent bg-teal-600 px-5 py-2 text-base font-medium text-white hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-teal-500 focus:ring-offset-2"
                     >
-                      ...
+                      <div role="status">
+                        <svg
+                          aria-hidden="true"
+                          className=" h-8 w-8 animate-spin fill-gray-50 text-teal-600 dark:text-gray-600"
+                          viewBox="0 0 100 101"
+                          fill="none"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                            fill="currentColor"
+                          />
+                          <path
+                            d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                            fill="currentFill"
+                          />
+                        </svg>
+                        <span className="sr-only">Loading...</span>
+                      </div>
                     </button>
                   ) : (
                     <button
                       disabled={invalid}
                       type="submit"
-                      className="flex w-full items-center justify-center rounded-md border border-transparent bg-teal-600 px-5 py-3 text-base font-medium text-white hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-teal-500 focus:ring-offset-2"
+                      className="flex w-full items-center justify-center gap-2 rounded-md border border-transparent bg-teal-600 px-5 py-3 text-base font-medium text-white hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-teal-500 focus:ring-offset-2"
                     >
-                      Iscrivimi
+                      <span>Iscrivimi</span>
                     </button>
                   )}
                 </div>
               </div>
               <div>
                 {success && (
-                  <div>Grazie per esserti iscritto! Ci sentiamo presto ✌️</div>
+                  <div className="text-xl font-bold text-gray-800">
+                    Grazie per esserti iscritto! Ci sentiamo presto 🚀
+                  </div>
+                )}
+                {error && (
+                  <div>
+                    Errore in fase di invio form, ti preghiamo di riprovare più
+                    tardi.
+                  </div>
                 )}
               </div>
               <div className="flex">
